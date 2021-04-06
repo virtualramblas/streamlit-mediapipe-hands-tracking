74320ac49be3ae5e8d2f4bd42a2e173d0c00369d
virtualramblas <guglielmo.iozzia@gmail.com>
2021-04-06 23:55:21 +0100

    Integration with Mediapipe Hands.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # streamlit-mediapipe-hands-tracking
-A [Streamlit](https://streamlit.io/) app for the Google's [MediaPipe Hands](https://google.github.io/mediapipe/solutions/hands).  
+A [Streamlit](https://streamlit.io/) app for Google [MediaPipe Hands](https://google.github.io/mediapipe/solutions/hands).  
 *Coming soon*
--- app.py
+++ app.py
@@ -1,15 +1,47 @@
+import cv2
+import mediapipe as mp
+import numpy as np
 import streamlit as st
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+def track_hands_in_images(file_list, max_num_hands, min_detection_confidence):
+    annotated_images = []
+    
+    with mp_hands.Hands(
+        static_image_mode=True,
+        max_num_hands=max_num_hands,
+        min_detection_confidence=min_detection_confidence) as hands:
+        for uploaded_file in file_list:
+            image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+            image = cv2.flip(image, 1)
+            results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            if not results.multi_hand_landmarks:
+                continue
+            image_height, image_width, _ = image.shape
+            annotated_image = image.copy()
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            annotated_images.append(annotated_image)
+            
+    return annotated_images
 
 st.write(
     """ # Hand Tracking in Pictures Using Google's MediaPipe Hands  """
 )
-output_placeholder = st.empty()
 
 st.sidebar.info("General settings")
-max_num_hands = st.sidebar.slider('Max number of hands:', 1, 6, 2, 1)
+max_num_hands = st.sidebar.slider('Max number of hands:', 1, 4, 2, 1)
 min_detection_confidence = st.sidebar.slider('Minimum detection confidence:', 0.1, 1.0, 0.5, 0.1)
 st.sidebar.info("Input")
 uploaded_files = st.sidebar.file_uploader("Upload JPG images of hands:", type=['jpg'], accept_multiple_files=True)
 if len(uploaded_files) > 0:
-    show_coordinates = st.sidebar.checkbox('Show index finger tips coordinates')
-    predict_button = st.sidebar.button('Track Hands')
+    show_coordinates = st.sidebar.checkbox('Show classification results')
+    track_button = st.sidebar.button('Track Hands')
+    if track_button:
+        annotated_images = track_hands_in_images(uploaded_files, max_num_hands, min_detection_confidence)
+        if len(annotated_images) > 0:
+            for annotated_image in annotated_images:
+                annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+                st.image(cv2.flip(annotated_image, 1))
