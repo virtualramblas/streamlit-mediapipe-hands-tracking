1e38cf806321aa8f66145f3383068ceadd59033c
virtualramblas <guglielmo.iozzia@gmail.com>
2021-04-07 23:20:00 +0100

    Implemented the classification results display.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ mp_hands = mp.solutions.hands
 
 def track_hands_in_images(file_list, max_num_hands, min_detection_confidence):
     annotated_images = []
+    multi_handednesses = []
     
     with mp_hands.Hands(
         static_image_mode=True,
@@ -18,14 +19,14 @@ def track_hands_in_images(file_list, max_num_hands, min_detection_confidence):
             results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
             if not results.multi_hand_landmarks:
                 continue
-            image_height, image_width, _ = image.shape
+            multi_handednesses.append(results.multi_handedness)
             annotated_image = image.copy()
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             annotated_images.append(annotated_image)
             
-    return annotated_images
+    return annotated_images, multi_handednesses
 
 st.write(
     """ # Hand Tracking in Pictures Using Google's MediaPipe Hands  """
@@ -37,11 +38,13 @@ min_detection_confidence = st.sidebar.slider('Minimum detection confidence:', 0.
 st.sidebar.info("Input")
 uploaded_files = st.sidebar.file_uploader("Upload JPG images of hands:", type=['jpg'], accept_multiple_files=True)
 if len(uploaded_files) > 0:
-    show_coordinates = st.sidebar.checkbox('Show classification results')
+    show_results = st.sidebar.checkbox('Show classification results')
     track_button = st.sidebar.button('Track Hands')
     if track_button:
-        annotated_images = track_hands_in_images(uploaded_files, max_num_hands, min_detection_confidence)
+        annotated_images, multi_handednesses = track_hands_in_images(uploaded_files, max_num_hands, min_detection_confidence)
         if len(annotated_images) > 0:
-            for annotated_image in annotated_images:
+            for idx, annotated_image in enumerate(annotated_images):
                 annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
                 st.image(cv2.flip(annotated_image, 1))
+                if show_results:
+                    st.write(str(multi_handednesses[idx]))
